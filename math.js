007f564fe05ba94c7a8601fb5e6e0822ef89937a
mtuck <Mtuck@una.edu>
2014-11-17 16:18:08 -0600

    added jquery stuff

--- math.js
+++ math.js
@@ -93,3 +93,24 @@ function addSponsor(){
 		return;
 	}
 }
+
+
+
+var tags = [ "c++", "java", "php", "coldfusion", "javascript", "asp", "ruby" ];
+
+$( "#autocomplete" ).autocomplete({
+
+  source: function( request, response ) {
+
+          var matcher = new RegExp( "^" + $.ui.autocomplete.escapeRegex( request.term ), "i" );
+
+          response( $.grep( tags, function( item ){
+
+              return matcher.test( item );
+
+          }) );
+
+      }
+
+});
+
