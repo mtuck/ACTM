3eed58aa6ade516a7e438b5e779fe3af203be497
mtuck <Mtuck@una.edu>
2014-11-17 16:27:23 -0600

    Revert "added jquery stuff"
    This reverts commit 007f564fe05ba94c7a8601fb5e6e0822ef89937a.
    
    Conflicts:
    	math.html

--- math.js
+++ math.js
@@ -93,24 +93,3 @@ function addSponsor(){
 		return;
 	}
 }
-
-
-
-var tags = [ "c++", "java", "php", "coldfusion", "javascript", "asp", "ruby" ];
-
-$( "#autocomplete" ).autocomplete({
-
-  source: function( request, response ) {
-
-          var matcher = new RegExp( "^" + $.ui.autocomplete.escapeRegex( request.term ), "i" );
-
-          response( $.grep( tags, function( item ){
-
-              return matcher.test( item );
-
-          }) );
-
-      }
-
-});
-
