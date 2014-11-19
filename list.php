584b59dd2654068b401d3893759b2bbf04c69dfb
mtuck <Mtuck@una.edu>
2014-11-18 20:45:59 -0600

    ajax works tested on sample database

--- list.php
+++ list.php
@@ -1,4 +1,31 @@
 <?php
+	//require_once 'login.php';
 
- echo "[\"martin\",\"Stephen\",\"Amy\"]";
+	$txt = "[\"\"";
+	$db_hostname = 'localhost';
+	$db_database = 'actm';
+	$db_username = 'root';
+	$db_password = '';
+	$connection = new mysqli($db_hostname,$db_username,$db_password,$db_database);
+	$query = "SELECT name FROM schools";
+	$result = $connection->query($query);
+
+	if(!$result) die($connection->error);
+
+	$rows = $result->num_rows;
+	
+	for($i = 0; $i < $rows; ++$i){
+		$result->data_seek($i);
+		$row = $result->fetch_array(MYSQLI_ASSOC);
+		$txt .= ",\"".$returnText = $row['name']."\"";
+		
+		
+	}
+	$txt.="]";
+
+	echo $txt;
+	$result->close();
+	$connection->close();
+
+
 ?>
