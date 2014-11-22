1f0749cf3c06ad7454be6ebcd9623504008d6170
abrown6 <abrown6@una.edu>
2014-11-22 00:11:26 -0600

    Following Martin's instructions
    Hope this works

--- validate.php
+++ validate.php
@@ -1,22 +1,57 @@
 <?php
 	
+	$sponsorName = filter_var($_POST['spon1Name'], FILTER_SANITIZE_STRING);
 	$to = filter_var($_POST['spon1Email'], FILTER_SANITIZE_EMAIL);
+	$sponsorPhone = filter_var($_POST['spon1phone'], FILTER_SANITIZE_PHONE);
+	
 	$comp = filter_var($_POST['compEnt'], FILTER_SANITIZE_NUMBER_INT);
 	$geom = filter_var($_POST['geomEnt'], FILTER_SANITIZE_NUMBER_INT);
 	$trig = filter_var($_POST['trigEnt'], FILTER_SANITIZE_NUMBER_INT);
 	$total = filter_var($_POST['total'], FILTER_SANITIZE_NUMBER_INT);
+	
 	$school = filter_var($_POST['autocomplete'], FILTER_SANITIZE_STRING);
+	$address = filter_var($_POST['schoolAddr'], FILTER_SANITIZE_STRING);
+	$city = filter_var($_POST['city'], FILTER_SANITIZE_STRING);
+	$zip = filter_var($_POST['schoolZip'], FILTER_SANITIZE_INT);
+	$state = filter_var($_POST['state'], FILTER_SANITIZE_STRING);
+	
 	$subject = "ACTM Receipt";
 	
-	$message = "Receipt for $school: \n
-					\t Comprehensive: $comp \n
-					\t Algebra II W/Trig: $trig \n
-					\t Geometry: $geom \n
-					\t Total: $total"
+	$message = "Receipt for ".$school.": \n
+					\t Comprehensive: ".$comp." \n
+					\t Algebra II W/Trig: ".$trig." \n
+					\t Geometry: ".$geom." \n
+					\t Total: ".$total;
 	
 	mail($to, $subject, $message);
 	
+<<<<<<< HEAD
+// Create connection
+	$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
+	} 
+//Insert records into database
+	$sql1 = "INSERT INTO ActmEntries (comprehensiveEntries, trigEntries, geometryEntries, total)
+			VALUES ($comp, $trig, $geom, $total)";
+	$sql2 = "INSERT INTO Schools (School_Name, Address, Zip_Code, School_State)
+			VALUES ($school, $address, $zip, $state)";
+	$sql3 = "INSERT INTO Sponsors (SponsorName, SponsorEmail, SPonsorPhone)
+			VALUES ($sponsorName, $to, $sponsorPhone);
+//Check record creation
+	if ($conn->query($sql1) === TRUE && $conn->query($sql2) === TRUE && $conn->query($sql3) === TRUE ) {
+		echo "New record created successfully";
+	} else {
+		echo "Error: " . $sql . "<br>" . $conn->error;
+	}
+//Close connecton
+	$conn->close();
+	
 	echo <<<RECEIPT
+=======
+echo <<< RECEIPT
+>>>>>>> origin/master
 	<!doctype html>
 	<html lang="en">
 	<head>
@@ -49,4 +84,5 @@
 	</body>
 	</html>
 RECEIPT;
+
 ?>
